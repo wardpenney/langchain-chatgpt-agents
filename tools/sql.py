8b7548ea6c2ab3e36d9a3bb1856c154449a625ce
Ward Penney <ward@wardpenney.com>
2025-03-12 14:39:54 -0600

    First tool agent

--- tools/sql.py
+++ tools/sql.py
@@ -1,2 +1,15 @@
 import sqlite3
 from langchain.tools import Tool
+
+conn = sqlite3.connect("db.sqlite")
+
+def run_sqlite_query(query):
+    cursor = conn.cursor()
+    cursor.execute(query)
+    return cursor.fetchall()
+
+run_query_tool = Tool.from_function(
+    name="run_sqlite_query",
+    description="Use this to run a query against the database",
+    func=run_sqlite_query,
+)
